f0c172cac3f56ea170c2d1aef953487919c91f97
vitorgaboardi <vitorgabo@hotmail.com>
2022-05-16 10:26:12 -0300

    Estruturando arquivo principal com funções

--- Storytelling Data Visualization: Gas Prices in Brazil/gas_prices_brazil.py
+++ Storytelling Data Visualization: Gas Prices in Brazil/gas_prices_brazil.py
@@ -1,7 +1,7 @@
 """
-gas_prices_brazil.py
-Arquivo para realizar a construção de uma imagem comparando o preço da gasolina em diferentes
-governos brasileiros
+Author: Vitor Gaboardi dos Santos
+Date: May 2022
+Image construction comparing gas prices in Brazil.
 """
 
 import pandas as pd
@@ -9,18 +9,58 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 style.use('fivethirtyeight')
 
+
+def read_data(path, sep=''):
+    """
+    Read a comma-separated values (csv) file and stores in a dataframe using pandas.
+
+    Args:
+    path: string. Any valid string path of the csv.
+    sep: string. Delimiter to use.
+    """
+    data_frame = pd.read_csv(path, sep=sep)
+    return data_frame
+
+def columns_processing(data_frame,select_columns,rename_columns=''):
+    """
+    Select and rename columns from dataframe.
+
+    Args:
+    data_frame: pandas dataFrame. Any valid string path of the csv.
+    select_columns: list of string. Delimiter to use.
+    rename_columns: dict of strings. Key: old column name. Value: new columns names.
+    """
+    new_data_frame = data_frame[select_columns].copy()
+    if rename_columns != '':
+        new_data_frame.rename(columns=rename_columns, inplace=True)
+    return new_data_frame
+
+def rolling_mean(data_frame,target_column,num_samples):
+    """
+    Computes moving average considering the column *target_column* and *num_samples* last samples.
+
+    Args:
+    data_frame: pandas dataFrame. Any valid string path of the csv.
+    select_columns: string. Column to compute the moving average.
+    rename_columns: int. Number of samples to consider in the moving average.
+    """
+    moving_average_result = data_frame[target_column].rolling(num_samples).mean()
+    return moving_average_result
+
 # read csv file
-dataframe = pd.read_csv('gas_prices_brazil.tsv', sep='\t')
+dataframe = read_data('gas_prices_brazil.tsv', sep='\t')
 
 # processing dataframe
-gas_prices = dataframe[['DATA INICIAL', 'ESTADO', 'PRODUTO', 'PREÇO MÉDIO REVENDA']].copy()
-gas_prices.rename(columns={'DATA INICIAL':'DATA', 'PREÇO MÉDIO REVENDA': 'PRECO'}, inplace=True)
+gas_prices = columns_processing(
+    dataframe,
+    ['DATA INICIAL', 'ESTADO', 'PRODUTO', 'PREÇO MÉDIO REVENDA'],
+    {'DATA INICIAL':'DATA', 'PREÇO MÉDIO REVENDA': 'PRECO'})
 gas_prices_rn = gas_prices[
     (gas_prices['ESTADO'] == 'RIO GRANDE DO NORTE') & (gas_prices['PRODUTO'] == 'GASOLINA COMUM')
     ].drop(columns=['ESTADO','PRODUTO'])
 gas_prices_rn['DATA'] = pd.to_datetime(gas_prices_rn['DATA'])
 gas_prices_rn.reset_index(drop=True, inplace=True)
-gas_prices_rn['MÉDIA MOVEL'] = gas_prices_rn['PRECO'].rolling(4).mean()
+gas_prices_rn['MÉDIA MOVEL'] = rolling_mean(gas_prices_rn,'PRECO',4)
 
 # separating data by president
 lula = gas_prices_rn.copy()[gas_prices_rn['DATA'].dt.year < 2011]
---
+++ Storytelling Data Visualization: Gas Prices in Brazil/test_gas_prices_brazil.py
@@ -0,0 +1,26 @@
+"""
+Author: Vitor Gaboardi dos Santos
+Date: May 2022
+Test File
+"""
+
+import pandas as pd
+from pandas.api.types import is_numeric_dtype
+from gas_prices_brazil import read_data, rolling_mean
+
+def test_read_data():
+    data = read_data("gas_prices_brazil.tsv")
+    assert data is None
+
+def test_columns_data():
+    data = read_data("gas_prices_brazil.tsv")
+    assert len(data.columns) > 0
+
+def test_has_data():
+    data = read_data("gas_prices_brazil.tsv")
+    assert len(data) > 0
+
+def test_rolling_mean():
+    data = read_data("gas_prices_brazil.tsv")
+    moving_average_result = rolling_mean(data,'PREÇO MÉDIO REVENDA',4)
+    assert is_numeric_dtype(moving_average_result) 
